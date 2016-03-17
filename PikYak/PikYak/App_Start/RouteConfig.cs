1a1027a68a418de11616d2772a716aa86f5d6862
Kenan Casey <kenancasey@gmail.com>
2016-03-17 09:05:24 -0600

    added basic project structure

--- PikYak/PikYak/App_Start/RouteConfig.cs
+++ PikYak/PikYak/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PikYak
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Yak", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
