5fc0bfe5abec190a49f22a6a78bfdf15679dcc8a
Kenan Casey <kenancasey@gmail.com>
2016-03-29 08:28:36 -0600

    mode goofy change

--- PikYak/PikYak/Controllers/AccountController.cs
+++ PikYak/PikYak/Controllers/AccountController.cs
@@ -20,6 +20,7 @@ namespace PikYak.Controllers
 
         public AccountController()
         {
+
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
