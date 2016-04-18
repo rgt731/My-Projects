86a04db02e9368f3cf44c6dee9f9bef39ac306e6
Kayjo4 <scotty.reynolds@students.fhu.edu>
2016-04-18 00:02:38 -0500

    Sort by likes and date. Does not work!!

---
+++ PikYak/PikYak/Controllers/SortController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PikYak.Controllers
+{
+    public class SortController : Controller
+    {
+        // GET: Sort
+        public ActionResult Index(string sortOrder)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.LikeSortParm = String.IsNullOrEmpty(sortOrder) ? "yak desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "Date desc" : "Date";
+
+            if (Request.HttpMethod == "GET")
+            {
+                searchString = currentFilter;
+            }
+            else
+            {
+                page = 1;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            var yak = from s in db.PikYak
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                yak = yak.Where(s => s.LastName.ToUpper().Contains(searchString.ToUpper())
+                                       || s.FirstMidName.ToUpper().Contains(searchString.ToUpper()));
+            }
+            switch (sortOrder)
+            {
+                case "yak desc":
+                    yak = yak.OrderByDescending(s => s.likes);
+                    break;
+                case "Date":
+                    yak = yak.OrderBy(s => s.PostDate);
+                    break;
+                case "Date desc":
+                    yak = yak.OrderByDescending(s => s.PostDate);
+                    break;
+            }
+            int pageSize = 4;
+            int pageNumber = (page ?? 1);
+            return View(yak.ToPagedList(pageNumber, pageSize));
+        }
+    }
+
+        // GET: Sort/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Sort/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sort/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sort/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Sort/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sort/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Sort/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
