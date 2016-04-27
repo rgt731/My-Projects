710b954a9602fe4cf3802eadd6badfc41e30be8e
Kayjo4 <scotty.reynolds@students.fhu.edu>
2016-04-26 22:31:01 -0500

    sort update

--- PikYak/PikYak/Controllers/YakController.cs
+++ PikYak/PikYak/Controllers/YakController.cs
@@ -28,32 +28,26 @@ namespace PikYak.Controllers
 
         public ActionResult Search(string sortOrder)
         {
-            
-            /*ViewBag.CurrentSort = sortOrder;
-            ViewBag.LikeSortParm = String.IsNullOrEmpty(sortOrder) ? "yak desc" : "";
-            ViewBag.DateSortParm = sortOrder == "Date" ? "Date desc" : "Date";*/
-
-            /* if (Request.HttpMethod == "GET")
-             {
-                 searchString = currentFilter;
-             }
-
-             ViewBag.CurrentFilter = searchString;*/
 
             var yaks = from s in db.Yaks.ToList()
                        select s;
 
             switch (sortOrder)
             {
-                /*case "yak desc":
-                    yak = yak.OrderByDescending(s => s.likes);
-                    break;*/
+
                 case "Date":
                     yaks = yaks.OrderBy(s => s.Timestamp);
                     break;
                 case "DateDesc":
                     yaks = yaks.OrderByDescending(s => s.Timestamp);
                     break;
+                case "Likes":
+                    yaks = yaks.OrderByDescending(s => s.likes);
+                    break;
+                /*case "location":
+                    yaks = yaks.OrderBy();
+                    break;*/
+
             }
 
             return View(yaks);
