20c9f262b1e75c4863e91e75bb372b8259fb6a03
rgt731 <robby.thompson@students.fhu.edu>
2016-05-03 10:17:47 -0500

    Editing layout

--- PikYak/PikYak/Scripts/main.js
+++ PikYak/PikYak/Scripts/main.js
@@ -1,3 +1,28 @@
-﻿$(document).ready(function () {
+﻿
+
+$(document).ready(function () {
+    //starts materialize up??
     $.material.init()
-});
+
+
+    //If someones browser supports geolocation do this
+    if (navigator.geolocation) {
+        //calls function success 
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+    else {
+        // browser doesn't support geolocation 
+    }
+
+
+    function success(position) {
+        //gets Id lat and long and assigns them to the current position for indiviudals latitude and longitude
+        document.getElementById("lat").innerHTML = position.coords.latitude;
+        document.getElementById("long").innerHTML = position.coords.longitude; 
+    }
+
+
+        function error (err) {
+            console.log(err.message);
+              }
+})
