603ed043eb75f5f5af500a1c7ebe51c7274a9293
rgt731 <robby.thompson@students.fhu.edu>
2016-05-09 22:17:44 -0500

    Deleting Things!!!!!
    I deleted things...

---
+++ PikYak/PikYak/Models/Report.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PikYak.Models
+{
+    public class Report
+    {
+
+        //create a convience constructor
+        public Report(int yakId)
+        {
+            //set intial value to Number
+            YakId = yakId;
+        }
+
+        //because this feels right to be first
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public string UserId { get; set; }
+
+        public int YakId { get; set; }
+
+    }
+}
